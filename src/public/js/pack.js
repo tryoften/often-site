353a2f66353a877d8bebc255f22ed99462b494dc
Luc Succes <luc@onoctober.com>
2016-06-01 20:23:59 -0400

    Adds link redirect

---
+++ src/public/js/pack.js
@@ -0,0 +1,37 @@
+deeplink.setup({
+    iOS: {
+        appName: "often-search-collect-share",
+        appId: "1053313047",
+    }
+});
+
+function filterItems(type) {
+    $('.item').fadeOut();
+    $('.item.'+type).fadeIn();
+}
+
+$('.tab').click(function(e) {
+    var selectedTab = $('.tab.selected');
+    var tab = $(e.target);
+
+    if (selectedTab.attr('id') === tab.attr('id')) {
+        return;
+    }
+
+    selectedTab.removeClass('selected');
+    tab.addClass('selected');
+
+    filterItems(tab.attr('id'));
+});
+
+$('.download-btn').click(function(e) {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        e.preventDefault();
+        var link = $(this).attr('href');
+        deeplink.open(link);
+    } else {
+       window.location = 'http://oftn.me/app';
+    }
+});
+
+filterItems($('.tab.selected').attr('id'));
